5562f25d9c6cdd535c65b7d98478e485d87b3c5b
Jett Thompson <jett.thompson.59@gmail.com>
2020-09-05 23:59:26 +1000

    added a blue screen to test swing

--- src/main/java/com/mycodefu/jnote/Start.java
+++ src/main/java/com/mycodefu/jnote/Start.java
@@ -1,7 +1,25 @@
 package com.mycodefu.jnote;
 
-public class Start {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.*;
+
+public class Start extends JPanel{
+/**
+	 * default uid
+	 */
+	private static final long serialVersionUID = 1L;
+
 public static void main(String[] args) {
-	System.out.println("Hello, World!");
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	Start window = new Start();
+	JFrame frame = new JFrame("window");
+	frame.setBounds(0, 0, screenSize.width, screenSize.height);
+	frame.add(window);
+	window.setBackground(Color.BLUE);
+	frame.setDefaultCloseOperation(3);
+	frame.setVisible(true);
 }
 }
